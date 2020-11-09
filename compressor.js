e672c9a093a24109bf9df5eb8fd16828998a299d
DschungelTim <tim.passgang@haw-hamburg.de>
2020-11-09 12:28:41 +0100

    test

---
+++ compressor.js
@@ -0,0 +1,74 @@
+let context = new AudioContext();
+let audio = new Audio("test.mp3");
+let source = context.createMediaElementSource(audio);
+let compressor = context.createDynamicsCompressor();
+let playButton = document.querySelector("#Playtaste");
+isPlaying = false;
+ReverOn = false;
+
+source.connect(compressor);
+compressor.connect(context.destination);
+
+
+
+compressor.threshold.value = -70;
+compressor.ratio.value = 12;
+compressor.knee.value = 15;
+compressor.attack.value = 0.16;
+compressor.release.value = 0.55;
+
+
+document.querySelector("#ThresholdSlider").addEventListener("input", function (e) {
+    let ThresholdValue = (this.value);
+    compressor.threshold.value = ThresholdValue;
+    document.getElementById("ThresholdOutput").innerHTML = ThresholdValue + "dB";
+    
+});
+
+document.querySelector("#RatioSlider").addEventListener("input", function (e) {
+    let RatioValue = (this.value);
+    compressor.ratio.value = RatioValue;
+    document.getElementById("RatioOutput").innerHTML = RatioValue;
+    
+});
+
+document.querySelector("#KneeSlider").addEventListener("input", function (e) {
+    let KneeValue = (this.value);
+    compressor.knee.value = KneeValue;
+    document.getElementById("KneeOutput").innerHTML = KneeValue;
+    
+});
+
+document.querySelector("#AttackSlider").addEventListener("input", function (e) {
+    let AttackValue = (this.value);
+    compressor.attack.value = AttackValue;
+    document.getElementById("AttackOutput").innerHTML = AttackValue + "ms";
+    
+});
+
+document.querySelector("#ReleaseSlider").addEventListener("input", function (e) {
+    let ReleaseValue = (this.value);
+    compressor.release.value = ReleaseValue;
+    document.getElementById("ReleaseOutput").innerHTML = ReleaseValue + "ms";
+    
+});
+
+playButton.addEventListener("click", function () {
+    if(isPlaying) {
+        audio.pause();
+        document.getElementById("Playtaste").innerHTML = "Play";
+    }
+    else {
+        audio.play();
+        document.getElementById("Playtaste").innerHTML = "Pause";   
+    }
+    isPlaying =! isPlaying;
+
+}); 
+
+audio.addEventListener("ended", function () {
+    isPlaying = false;
+    document.getElementById("Playtaste").innerHTML = "Play";
+
+
+});
---
+++ reverb.js
@@ -0,0 +1,50 @@
+let context = new AudioContext();
+let sound = new Audio("test.p3");
+let audio = context.createMediaElementSource(sound);
+let convolver = context.createConvolver();
+let isPlaying = false;
+
+audio.connect(convolver);
+convolver.connect(context.destination);
+
+document.querySelector("#playbutton").addEventListener("click", function (e) {
+     auslesen();
+     if (isPlaying) {
+          sound.pause();
+          document.querySelector("#playbutton").innerHTML = "Play";
+     }
+
+     else {
+          var auswahl = document.getElementById("Auswahl").value; 
+          fetch("impulseResponses/" + auswahl +".wav")
+          .then(response => response.arrayBuffer())
+          .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+          .then(audioBuffer => {
+          convolver.buffer = audioBuffer;
+          convolver.normalize = true;
+     })
+     .catch(console.error);
+          sound.play();
+     
+     document.querySelector("#playbutton").innerHTML = "Stop";
+     }
+     isPlaying =! isPlaying;
+ 
+});
+
+
+
+sound.addEventListener("ended", function () {
+     isPlaying = false;
+     document.getElementById("playbutton").innerHTML = "Play";
+ 
+ });
+
+
+function auslesen() {
+     var auswahl = document.getElementById("Auswahl").value; 
+     document.getElementById("output").innerHTML = auswahl; 
+}
+
+
+     
